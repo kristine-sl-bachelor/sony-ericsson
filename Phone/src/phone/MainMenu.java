c2223a2b4415c3963b42d314fd47353261bc8626
Kristine Sundt Lorentzen <sunkri12@nith.no>
2013-04-25 12:54:39 +0200

    Jobbet med meny

---
+++ Phone/src/phone/MainMenu.java
@@ -0,0 +1,18 @@
+package phone;
+
+public class MainMenu extends Menu{
+
+	private static MenuItem[] items = {
+			new MenuItem("Item 1"), 
+			new MenuItem("Item 2"), 
+			new MenuItem("Item 3"), 
+			new MenuItem("Item 4"), 
+			new MenuItem("Item 5"), 
+	};
+	
+	public MainMenu() {
+		
+		super(items); 
+		
+	}
+}
--- Phone/src/phone/Menu.java
+++ Phone/src/phone/Menu.java
@@ -1,38 +1,22 @@
 package phone;
 
-import java.awt.Color;
-import java.awt.Font;
 import java.awt.GridLayout;
 
 import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.SwingConstants;
 
 public class Menu extends JPanel{
 
-	public Menu() {
+	public Menu(MenuItem[] items) {
 		
-		String[] menuItems = {
-			"Item 1", 
-			"Item 2", 
-			"Item 3", 
-			"Item 4", 
-			"Item 5"
-				
-		}; 
+		this.setLayout(new GridLayout(items.length, 1)); 
 		
-		this.setLayout(new GridLayout(menuItems.length, 1)); 
-		
-		for(String item : menuItems) {
-			JTextField tf = new JTextField(item); 
-			tf.setHorizontalAlignment(SwingConstants.CENTER); 
-			tf.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 16)); 
-			tf.setBackground(Color.GRAY); 
-			tf.setForeground(Color.WHITE); 
-			tf.setEditable(false); 
-			add(tf); 
+		for (MenuItem item : items) {
+			
+			this.add(item); 
+			
 		}
 		
+		items[0].selected(true);
 	}
 	
 }
